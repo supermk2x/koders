986d1df9c1fb3365cae584253dbd44d1722a2982
supermk2x <149475430+supermk2x@users.noreply.github.com>
2024-03-31 16:34:23 +0000

    container config done

---
+++ 01_basics/js1.js
@@ -0,0 +1,10 @@
+console.log("dgdfbdfbfdb")
+name="sunny";
+let proff=new String("engineer");
+
+console.log(`my name is ${name.toUpperCase()} and I am an ${proff}`);
+console.log(name);
+
+
+let name1=name.slice(-4,1);
+console.log(name1);
